01a13753626716f4de5ecf2bc047af4bd17976f3
Yvan <yvan.gadeau@gmail.com>
2013-10-28 11:22:08 +0100

    Suppression du DefaultController dans le AppBundle + dans le AccueilController, ajout du code permettant de préremplir le formulaire de login

--- src/SmartUnity/AppBundle/Controller/AccueilController.php
+++ src/SmartUnity/AppBundle/Controller/AccueilController.php
@@ -9,11 +9,58 @@ use Symfony\Component\Security\Core\SecurityContext;
 
 class AccueilController extends Controller
 {
-    public function indexAction()
-    {
+    public function indexAction(Request $request)
+
+//Le code de la function indexAction Permet de récupérer la session et de préremplir le formulaire de login.    
+            {
         
-        return $this->render('SmartUnityAppBundle::Accueil.html.twig');
         
+        /** @var $session \Symfony\Component\HttpFoundation\Session\Session */
+        $session = $request->getSession();
+
+        // get the error if any (works with forward and redirect -- see below)
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } elseif (null !== $session && $session->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = '';
+        }
+
+        if ($error) {
+            // TODO: this is a potential security risk (see http://trac.symfony-project.org/ticket/9523)
+            $error = $error->getMessage();
+        }
+        // last username entered by the user
+        $lastUsername = (null === $session) ? '' : $session->get(SecurityContext::LAST_USERNAME);
+
+        $csrfToken = $this->container->has('form.csrf_provider')
+            ? $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate')
+            : null;
+
+        return $this->renderLogin(array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+            'csrf_token' => $csrfToken,
+        ));
     }
+
+    /**
+     * Renders the login template with the given parameters. Overwrite this function in
+     * an extended controller to provide additional data for the login template.
+     *
+     * @param array $data
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function renderLogin(array $data)
+    {
+        $template = sprintf('SmartUnityAppBundle::Accueil.html.twig');
+
+        return $this->container->get('templating')->renderResponse($template, $data);
+    }
+//        return $this->render('SmartUnityAppBundle:Default:index.html.twig');
+
     
-}
+    }
