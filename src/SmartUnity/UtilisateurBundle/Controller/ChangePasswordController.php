03e9aec5c6723d353c69986a1d22cc2eecb94dc2
Yvan <yvan.gadeau@gmail.com>
2013-11-26 10:43:59 +0100

    "layout.twig" supprimé. "ChangePasswordController" forward maintenant vers UtilisateurController, c'est plus propre.

--- src/SmartUnity/UtilisateurBundle/Controller/ChangePasswordController.php
+++ src/SmartUnity/UtilisateurBundle/Controller/ChangePasswordController.php
@@ -20,23 +20,21 @@ use FOS\UserBundle\Model\UserInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
-use FOS\UserBundle\Controller\ChangePasswordController as BaseController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class ChangePasswordController extends BaseController {
+//use FOS\UserBundle\Controller\ChangePasswordController as BaseController;
+
+class ChangePasswordController extends Controller {
 
     /**
      * Change user password
      */
     public function changePasswordAction(Request $request) {
 
-        $session = $request->getSession();
         $user = $this->container->get('security.context')->getToken()->getUser();
         if (!is_object($user) || !$user instanceof UserInterface) {
             throw new AccessDeniedException('This user does not have access to this section.');
         }
-
-//        Création du formulaire des préférences
-        $form_pref = $this->container->get('form.factory')->create('smartunity_user_preference', $user);
 
         /** @var $dispatcher \Symfony\Component\EventDispatcher\EventDispatcherInterface */
         $dispatcher = $this->container->get('event_dispatcher');
@@ -78,9 +76,9 @@ class ChangePasswordController extends BaseController {
         }
 
 
-        return $this->container->get('templating')->renderResponse(
-                        'FOSUserBundle:ChangePassword:changePassword.html.' . $this->container->getParameter('fos_user.template.engine'), array('form_pref' => $form_pref->createView(),
-                    'form_password' => $form_password->createView(),
+        return $this->forward(
+                        'SmartUnityUtilisateurBundle:Utilisateur:index', array(
+                            'formPassword' => $form_password->createView(),
         ));
     }
 
--- src/SmartUnity/UtilisateurBundle/Controller/UtilisateurController.php
+++ src/SmartUnity/UtilisateurBundle/Controller/UtilisateurController.php
@@ -1,90 +1,97 @@
 <?php
 
 namespace SmartUnity\UtilisateurBundle\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\QueryBuilder;
 
-class UtilisateurController extends Controller
-{
-    public function indexAction(Request $request)
-    {
+class UtilisateurController extends Controller {
+
+    public function indexAction(Request $request, $formPassword = null) {
 
         $user = $this->container->get('security.context')->getToken()->getUser();
-       
-        $form_pref=$this->createForm('smartunity_user_preference', $user);
+
+        $form_pref = $this->createForm('smartunity_user_preference', $user);
         $em = $this->getDoctrine()->getEntityManager();
-        
-        $membreRepository = $this->getDoctrine()
-                            ->getManager()
-                            ->getRepository('SmartUnityAppBundle:membre');
 
-        $userid=$user->getId();
-        $smartreponse = $membreRepository->getSmartReponses($userid);  
+        $membreRepository = $this->getDoctrine()
+                ->getManager()
+                ->getRepository('SmartUnityAppBundle:membre');
+
+        $userid = $user->getId();
+        $smartreponse = $membreRepository->getSmartReponses($userid);
         $remuneration = 0;
-        $remuneration= $membreRepository->getRemuneration($userid);
-        //$remuneration = 0;
-        return $this->render('SmartUnityUtilisateurBundle:Profile:show.html.twig', array('form_pref'=> $form_pref->createView(),
-                                                                                        'smartrep'=> $smartreponse,
-                                                                                        'remuneration'=> $remuneration 
-                                                                                    ));
+        $remuneration = $membreRepository->getRemuneration($userid);
+
+        if (isset($formPassword)) {
+
+            return $this->render('SmartUnityUtilisateurBundle:ChangePassword:changePassword.html.twig', array(
+                        'form_pref' => $form_pref->createView(),
+                        'smartrep' => $smartreponse,
+                        'remuneration' => $remuneration,
+                        'form_password' => $formPassword
+            ));
+        } else {
+            return $this->render('SmartUnityUtilisateurBundle:Profile:show.html.twig', array(
+                        'form_pref' => $form_pref->createView(),
+                        'smartrep' => $smartreponse,
+                        'remuneration' => $remuneration,
+            ));
+        }
     }
 
-    public function indexWithEditInfosAction()
-    {
+    public function indexWithEditInfosAction() {
         $user = $this->container->get('security.context')->getToken()->getUser();
 
-        $form_pref=$this->createForm('smartunity_user_preference', $user);
-        $form_infos=$this->createForm('smartunity_user_informations', $user);
+        $form_pref = $this->createForm('smartunity_user_preference', $user);
+        $form_infos = $this->createForm('smartunity_user_informations', $user);
 
 
         $membreRepository = $this->getDoctrine()
-                            ->getManager()
-                            ->getRepository('SmartUnityAppBundle:membre');
+                ->getManager()
+                ->getRepository('SmartUnityAppBundle:membre');
 
-        $userid=$user->getId();
-        $smartreponse = $membreRepository->getSmartReponses($userid); 
-        $remuneration = $membreRepository->getRemuneration($userid); 
-        return $this->render('SmartUnityUtilisateurBundle:Profile:edit.html.twig', array('form_infos'=> $form_infos->createView(),'form_pref'=> $form_pref->createView(), 'smartrep'=> $smartreponse, 'remuneration'=> $remuneration));
-    }    
-
-    public function setPrefAction(){
-
-            $em=$this->getDoctrine()->getManager();
-            $user = $this->container->get('security.context')->getToken()->getUser();
-            $form=$this->createForm('smartunity_user_preference', $user);
-            $form->bind($this->getRequest());
-
-           if($form->isValid()){
-
-            $em=$this->getDoctrine()->getManager();
-            $em->persist($user);
-            $em->flush(); 
-            }
-
-        
-            return $this->redirect($this->generateUrl('smart_unity_utilisateur_homepage'));
-
+        $userid = $user->getId();
+        $smartreponse = $membreRepository->getSmartReponses($userid);
+        $remuneration = $membreRepository->getRemuneration($userid);
+        return $this->render('SmartUnityUtilisateurBundle:Profile:edit.html.twig', array('form_infos' => $form_infos->createView(), 'form_pref' => $form_pref->createView(), 'smartrep' => $smartreponse, 'remuneration' => $remuneration));
     }
-    
-    public function setInfosAction(){
 
-            $em=$this->getDoctrine()->getManager();
-            $user = $this->container->get('security.context')->getToken()->getUser();
-            $form=$this->createForm('smartunity_user_informations', $user);
-            $form->bind($this->getRequest());
+    public function setPrefAction() {
 
-           if($form->isValid()){
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        $form = $this->createForm('smartunity_user_preference', $user);
+        $form->bind($this->getRequest());
 
-            $em=$this->getDoctrine()->getManager();
+        if ($form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
             $em->persist($user);
-            $em->flush(); 
-            }
-               
-            
+            $em->flush();
+        }
+
+
         return $this->redirect($this->generateUrl('smart_unity_utilisateur_homepage'));
-
     }
 
-    
+    public function setInfosAction() {
+
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        $form = $this->createForm('smartunity_user_informations', $user);
+        $form->bind($this->getRequest());
+
+        if ($form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+        }
+
+
+        return $this->redirect($this->generateUrl('smart_unity_utilisateur_homepage'));
+    }
+
 }
