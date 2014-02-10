c6de89de417fc2dadcb01bc8432565d3fec2bd63
epitomz <antonin.delbos@gmail.com>
2014-02-10 21:46:38 +0100

    CSS formulaires : poser une question + register

--- src/SmartUnity/QuestionReponseBundle/Controller/QuestionReponseController.php
+++ src/SmartUnity/QuestionReponseBundle/Controller/QuestionReponseController.php
@@ -612,18 +612,15 @@ p($listeQuestions);
                     'class' => 'SmartUnityAppBundle:marque',
                     'property' => 'nom',
                     'required' => false,
-                    'empty_value' => 'Choisissez',
                     'empty_data' => NULL))
                 ->add('os', 'entity', array(
                     'class' => 'SmartUnityAppBundle:os',
                     'property' => 'nom',
                     'required' => false,
-                    'empty_value' => 'Choisissez',
                     'empty_data' => NULL))
                 ->add('typeQuestion', 'entity', array(
                     'class' => 'SmartUnityAppBundle:typeQuestion',
                     'property' => 'nom',
-                    'empty_value' => 'Choisissez une option',
                     'required' => false))
                 ->add('remuneration', 'integer', array('attr' => array('min' => 10, 'max' => ($dotationMax))))
                 ->add('save', 'submit', array('label' => 'Poser ma question'))
