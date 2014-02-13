9f377506391aaae6f83e88d9790d8ca4b3203b78
Yvan <yvan.gadeau@gmail.com>
2014-02-13 13:23:11 +0100

    Signaler un bug OK

--- src/SmartUnity/AppBundle/Controller/AccueilController.php
+++ src/SmartUnity/AppBundle/Controller/AccueilController.php
@@ -249,31 +249,27 @@ class AccueilController extends Controller {
             $form->handleRequest($request);
             $sujet= $form->get('sujet')->getData();
             $sujetMail = "[Bug]: " . $sujet;
-            $expediteurMail = $form->get('email')->getData();
-            $destinataireMail="ne-pas-repondre@smartunity.fr";
-            $contenu = "Bug signalé par ".$$form->get('email')->getData()."<br/>". $form->get('message')->getData();
-            $message = \Swift_Message::newInstance()
+            $contenu = "Bug signalé par ".$form->get('email')->getData()."<br/>". $form->get('message')->getData();
+            $message1 = \Swift_Message::newInstance()
                         ->setContentType('text/html')
                         ->setSubject($sujetMail)
-                        ->setFrom($expediteurMail)
-                        ->setTo($destinataireMail)
+                        ->setFrom($form->get('email')->getData())
+                        ->setTo("contact@smartunity.fr")
                         ->setBody($contenu);
-            $this->get('mailer')->send($message);
+            $this->get('mailer')->send($message1);
 
             $confirmation="Votre bug a bien été signalé à l'équipe Smart'Unity";
 
-            $sujet= $form->get('sujet')->getData();
-            $sujetMail = "Confirmation contact Smart'Unity";
+            $sujetMail = "Confirmation signal d'un bug à Smart'Unity";
             $expediteurMail = "ne-pas-repondre@smartunity.fr";
-            $destinataireMail =$form->get('email')->getData();
-            $contenu = "Bonjour, <br/> Votre bug : " . $sujet ." nous a bien été envoyé. <br/> Nous vous remercions d'avoir signaler ce bug <br/> Nous allons tout mettre en oeuvre pour corriger cela le plus rapidement. <br/> <br/> L'équipe Smart'Unity";
-            $message = \Swift_Message::newInstance()
+            $contenu = "Bonjour, <br/> Votre bug : " . $sujet ." nous a bien été envoyé. <br/> Nous vous remercions d'avoir signalé ce bug <br/> Nous allons tout mettre en oeuvre pour corriger cela le plus rapidement. <br/> <br/> L'équipe Smart'Unity";
+            $message2 = \Swift_Message::newInstance()
                         ->setContentType('text/html')
                         ->setSubject($sujetMail)
                         ->setFrom($expediteurMail)
-                        ->setTo("contact@smartunity.fr")
+                        ->setTo($form->get('email')->getData())
                         ->setBody($contenu);
-            $this->get('mailer')->send($message);
+            $this->get('mailer')->send($message2);
         $form = $this->createFormBuilder()
         ->add('email', 'email', array(
             'required' => true))
