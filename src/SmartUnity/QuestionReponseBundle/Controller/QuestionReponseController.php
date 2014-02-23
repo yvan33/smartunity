01f33c06da664cb95c169228c656c63060b2ce54
Florent <florent.marty19@gmail.com>
2014-02-23 19:58:02 +0100

    Modification slugify pour prendre en compte les caractères spéciaux

--- src/SmartUnity/QuestionReponseBundle/Controller/QuestionReponseController.php
+++ src/SmartUnity/QuestionReponseBundle/Controller/QuestionReponseController.php
@@ -736,12 +736,14 @@ class QuestionReponseController extends Controller {
     }
 
     public function slugify($str) {
-        $search = array('', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+        $str = iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', $str);
+        $search = array('', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '',);
         $replace = array('s', 't', 's', 't', 's', 't', 's', 't', 'i', 'a', 'a', 'i', 'a', 'a', 'e', 'E');
         $str = str_ireplace($search, $replace, strtolower(trim($str)));
         $str = preg_replace('/[^\w\d\-\ ]/', '', $str);
         $str = str_replace(' ', '-', $str);
         return preg_replace('/\-{2,}/', '-', $str);
+
     }
 
     public function addReponseAction($slug) {
